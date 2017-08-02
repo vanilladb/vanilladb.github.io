d6fda6fb7133453453ed1f4a6f6f9d8b1da2e292
zulu <e24p21@gmail.com>
2017-08-02 16:48:57 +0800

    auto update version number

--- js/vanilladb.js
+++ js/vanilladb.js
@@ -25,3 +25,22 @@ $(document).ready(function(){
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
+
+// jQuery for version number
+var url = 'https://script.google.com/macros/s/AKfycbzAeQ--a_RM3kDJchPOjvOACD4Bm8C8daFHHZj4aCvN3kaeaxE/exec';
+
+$(document).ready($.get(url,
+    function (data) {
+        $('.coreVer > div ').css({"opacity":"0"})
+        setTimeout( function() {
+        $('.coreVer > div').text("Version: "+data.core);
+        $('.coreVer > div').css({"opacity":"100"})
+        }, 500);
+        
+        $('.commVer > div ').css({"opacity":"0"})
+        setTimeout( function() {
+        $('.commVer > div').text("Version: "+data.comm);
+        $('.commVer > div').css({"opacity":"100"})
+        }, 500);
+    }
+));
